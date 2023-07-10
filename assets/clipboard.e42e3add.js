851f306d0d9bd2f9071d3e11eadd4eb1e555b5e8
edisonh <edison.h.cr@gmail.com>
2023-07-10 17:43:24 +0000

    Deploying to gh-pages from @ edisonh/pp@c21d3824b5bbb53e3be323cf6b04b57de5f132fc 🚀

--- assets/clipboard.e42e3add.js
+++ assets/clipboard.e42e3add.js
@@ -1,4 +1,4 @@
-import{au as Y,av as G}from"./vendor.b7de2019.js";var O={exports:{}};/*!
+import{av as Y,aw as G}from"./vendor.16a1b8af.js";var O={exports:{}};/*!
  * clipboard.js v2.0.8
  * https://clipboardjs.com/
  *
