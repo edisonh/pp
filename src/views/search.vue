a1c715eb89d4c3f34a32208b20f2053f969c7e02
edisonH <edison.h.cr@gmail.com>
2023-07-31 00:00:48 +0800

    Update search interval

--- src/views/search.vue
+++ src/views/search.vue
@@ -373,7 +373,7 @@ const deepScan = async (parentIds: string[]) => {
     if(files[i].kind == 'drive#folder') {
       const localFile = await findFileById(files[i].id)
       if (!localFile.scanned) {
-        await sleep(500)
+        //await sleep(500)
         await deepScan(parentIds.concat([files[i].id]))
       }
     }
