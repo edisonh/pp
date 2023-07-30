f5f8fd799a157700b11308a44c70bc50ab40b741
edisonH <edison.h.cr@gmail.com>
2023-07-30 12:28:45 +0800

    Updates api auth

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -128,9 +128,9 @@ instance.interceptors.response.use(response => {
           const client = JSON.parse(window.localStorage.getItem('pikpakClient')||'{}')
           return instance.post('https://user.mypikpak.com/v1/auth/signin', {
             "captcha_token": "",
-            //"client_id": "YNxT9w7GMdWvEOKa",
+            "client_id": "YNxT9w7GMdWvEOKa",
             "client_secret": "dbw2OtmVEeuUvIptb1Coyg",
-            "client_id": client.clientId,
+            //"client_id": client.clientId,
             ...loginDataJson
           })
             .then((res:any) => {
--- src/views/login.vue
+++ src/views/login.vue
@@ -76,9 +76,9 @@ const loginPost = () => {
   const client = JSON.parse(window.localStorage.getItem('pikpakClient')||'{}')
   http.post('https://user.mypikpak.com/v1/auth/signin', {
     "captcha_token": "",
-    //"client_id": "YNxT9w7GMdWvEOKa",
+    "client_id": "YNxT9w7GMdWvEOKa",
     "client_secret": "dbw2OtmVEeuUvIptb1Coyg",
-    "client_id": client.clientId,
+    //"client_id": client.clientId,
     ...loginData.value
   })
     .then((res:any) => {
