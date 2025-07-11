c45f07aa09bbb4ae568a8ae7a794628eabed50aa
edisonH <edison.h.cr@gmail.com>
2025-07-11 19:19:06 +0800

    add saving data set ok

--- src/views/list.vue
+++ src/views/list.vue
@@ -1098,7 +1098,7 @@ const handlePikFile = async (file: any) => {
         }
       } else if (filesList.value[i].kind === 'drive#folder' && filesList.value[i].children) {
         for (let j in filesList.value[i].children) {
-          handlePikFile(j)
+          handlePikFile(filesList.value[i].children[j])
           const info = await getExistInfo(filesList.value[i].children[j].id, filesList.value[i].children[j].name, Number(filesList.value[i].children[j].size))
           filesList.value[i].children[j].exist = info
           if (!info.exist) {
